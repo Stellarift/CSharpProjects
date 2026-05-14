a398ee9a7e3059c612d6c2033fa854d01fdc03b3
Stellarift <filippov2888@gmail.com>
2026-05-14 21:39:01 +0900

    Add files via upload

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] originalArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] filterArray = { 2, 4, 6, 8, 10 };
+
+        int[] result = FilterArray(originalArray, filterArray);
+
+        Console.WriteLine("Отфильтрованный массив:");
+        foreach (int item in result)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    static int[] FilterArray(int[] originalArray, int[] filterArray)
+    {
+        List<int> filteredList = new List<int>();
+
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            for (int j = 0; j < filterArray.Length; j++)
+            {
+                if (originalArray[i] == filterArray[j])
+                {
+                    filteredList.Add(originalArray[i]);
+                    break;
+                }
+            }
+        }
+
+        return filteredList.ToArray();
+    }
+}
