d63320dace5514dfaf835fd087d44f56a307f8ce
Stellarift <filippov2888@gmail.com>
2026-05-28 18:06:01 +0900

    Add files via upload

---
+++ 21.03(1)/PropertyChanged/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+// Делегат
+public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+
+// Аргументы события
+public class PropertyEventArgs : EventArgs
+{
+    public string PropertyName { get; }
+    
+    public PropertyEventArgs(string propertyName)
+    {
+        PropertyName = propertyName;
+    }
+}
+
+// Интерфейс
+interface IPropertyChanged
+{
+    event PropertyEventHandler? PropertyChanged;
+}
+
+// Класс, реализующий интерфейс
+class User : IPropertyChanged
+{
+    private string _name = string.Empty;
+    private int _age;
+
+    public event PropertyEventHandler? PropertyChanged;
+
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+
+    public int Age
+    {
+        get { return _age; }
+        set
+        {
+            _age = value;
+            OnPropertyChanged(nameof(Age));
+        }
+    }
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyEventArgs(propertyName));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        User user = new User();
+
+        // Подписываемся на событие
+        user.PropertyChanged += (sender, e) =>
+        {
+            Console.WriteLine($"Свойство '{e.PropertyName}' было изменено");
+        };
+
+        // Меняем свойства
+        user.Name = "Иван";
+        user.Age = 30;
+        user.Name = "Петр";
+    }
+}
---
+++ 21.03(1)/Statistics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Вот дом, Который построил Джек. А это пшеница, " +
+                      "Которая в темном чулане хранится В доме, " +
+                      "Который построил Джек. А это веселая птица-синица, " +
+                      "Которая часто ворует пшеницу, Которая в темном чулане хранится " +
+                      "В доме, Который построил Джек.";
+
+        string cleanText = text.ToLower();
+        char[] separators = { ' ', ',', '.', '!', '?', ';', ':', '\n', '\r', '-', '—' };
+        string[] words = cleanText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        // Подсчитываем частоту слов
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        foreach (string word in words)
+        {
+            if (dictionary.ContainsKey(word))
+                dictionary[word]++;
+            else
+                dictionary[word] = 1;
+        }
+
+        // Выводим таблицу
+        Console.WriteLine("Статистика слов в тексте:\n");
+        Console.WriteLine("Слово\t\t\tКоличество");
+        Console.WriteLine("---------------------------------");
+
+        foreach (KeyValuePair<string, int> item in dictionary)
+        {
+            Console.WriteLine($"{item.Key,-20}\t{item.Value}");
+        }
+
+        Console.WriteLine($"\nВсего слов: {words.Length}, из них уникальных: {dictionary.Count}");
+    }
+}
