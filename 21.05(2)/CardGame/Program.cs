e76b6bd5ccdc890480b7f86fa159804de01f3268
Stellarift <filippov2888@gmail.com>
2026-05-28 18:57:12 +0900

    Add files via upload

---
+++ 21.05(2)/CardGame/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Класс Карта
+class Card
+{
+    public string Rank { get; private set; }
+    public string Suit { get; private set; }
+
+    public Card(string rank, string suit)
+    {
+        Rank = rank;
+        Suit = suit;
+    }
+
+    public int GetValue()
+    {
+        switch (Rank)
+        {
+            case "6":
+             return 6;
+            case "7":
+             return 7;
+            case "8":
+             return 8;
+            case "9":
+             return 9;
+            case "10":
+             return 10;
+            case "Валет":
+             return 11;
+            case "Дама":
+             return 12;
+            case "Король":
+             return 13;
+            case "Туз":
+             return 14;
+            default:
+            return 0;
+        }
+    }
+
+    public bool IsHigherThan(Card other)
+    {
+        // Шестёрка не забирает туза
+        if (this.Rank == "6" && other.Rank == "Туз")
+            return false;
+        if (other.Rank == "6" && this.Rank == "Туз")
+            return true;
+        
+        return GetValue() > other.GetValue();
+    }
+
+    public override string ToString()
+    {
+        return $"{Rank}{Suit}";
+    }
+}
+
+// Класс Игрок
+class Player
+{
+    public string Name { get; private set; }
+    private Queue<Card> _cards = new Queue<Card>();
+
+    public Player(string name)
+    {
+        Name = name;
+    }
+
+    public int CardCount => _cards.Count;
+
+    public void AddCard(Card card)
+    {
+        _cards.Enqueue(card);
+    }
+
+    public void AddCards(List<Card> cards)
+    {
+        foreach (Card c in cards)
+            _cards.Enqueue(c);
+    }
+
+    public Card PlayCard()
+    {
+        return _cards.Dequeue();
+    }
+
+    public void ShowCards()
+    {
+        Console.Write($"{Name} ({CardCount} карт): ");
+        foreach (Card c in _cards)
+            Console.Write(c + " ");
+        Console.WriteLine();
+    }
+}
+
+// Класс Игра
+class CardGame
+{
+    private List<Player> _players = new List<Player>();
+    private List<Card> _deck = new List<Card>();
+
+    public CardGame(string[] playerNames)
+    {
+        foreach (string name in playerNames)
+            _players.Add(new Player(name));
+
+        CreateDeck();
+        Shuffle();
+        Deal();
+    }
+
+    private void CreateDeck()
+    {
+        string[] ranks = { "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
+        string[] suits = { "♠", "♥", "♣", "♦" };
+
+        foreach (string suit in suits)
+            foreach (string rank in ranks)
+                _deck.Add(new Card(rank, suit));
+    }
+
+    private void Shuffle()
+    {
+        Random rnd = new Random();
+        for (int i = _deck.Count - 1; i > 0; i--)
+        {
+            int j = rnd.Next(i + 1);
+            Card temp = _deck[i];
+            _deck[i] = _deck[j];
+            _deck[j] = temp;
+        }
+    }
+
+    private void Deal()
+    {
+        int cardsPerPlayer = _deck.Count / _players.Count;
+        int index = 0;
+
+        foreach (Player p in _players)
+        {
+            for (int i = 0; i < cardsPerPlayer; i++)
+                p.AddCard(_deck[index++]);
+        }
+    }
+
+    public void Play()
+    {
+        int round = 1;
+        int totalCards = _deck.Count;
+
+        while (true)
+        {
+            // Проверяем победителя
+            foreach (Player p in _players)
+            {
+                if (p.CardCount == totalCards)
+                {
+                    Console.WriteLine($"\n*** ПОБЕДИТЕЛЬ: {p.Name} ***");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"\nРаунд {round++}");
+
+            List<Card> playedCards = new List<Card>();
+            List<Card> stake = new List<Card>();
+
+            // Каждый игрок кладёт карту
+            foreach (Player p in _players)
+            {
+                if (p.CardCount > 0)
+                {
+                    Card card = p.PlayCard();
+                    playedCards.Add(card);
+                    stake.Add(card);
+                    Console.WriteLine($"{p.Name} кладёт {card}");
+                }
+            }
+
+            // Определяем победителя раунда
+            int winnerIndex = 0;
+            for (int i = 1; i < playedCards.Count; i++)
+            {
+                if (playedCards[i].IsHigherThan(playedCards[winnerIndex]))
+                    winnerIndex = i;
+            }
+
+            Console.WriteLine($"Забирает {_players[winnerIndex].Name}");
+            _players[winnerIndex].AddCards(stake);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Игрок 1", "Игрок 2" };
+        CardGame game = new CardGame(names);
+        game.Play();
+    }
+}
---
+++ 21.05(2)/RaceGame/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+// Делегат для старта гонки
+public delegate void StartRaceDelegate();
+
+// Абстрактный класс Автомобиль
+abstract class Car
+{
+    public string Name { get; protected set; }
+    public double Position { get; protected set; }
+    protected double _speed;
+    protected Random _rnd = new Random();
+
+    public event Action<Car>? FinishEvent;
+
+    public Car(string name)
+    {
+        Name = name;
+        Position = 0;
+        _speed = 0;
+    }
+
+    public void Go()
+    {
+        if (Position >= 100) return;
+        
+        _speed = _rnd.Next(5, 20);
+        Position += _speed;
+
+        if (Position >= 100 && FinishEvent != null)
+            FinishEvent(this);
+    }
+
+    public abstract void ShowStatus();
+}
+
+// Спортивный автомобиль
+class SportCar : Car
+{
+    public SportCar(string name) : base(name) { }
+
+    public override void ShowStatus()
+    {
+        Console.WriteLine($"Спортивный {Name}: {Math.Min(Position, 100):F1} км");
+    }
+}
+
+// Легковой автомобиль
+class PassengerCar : Car
+{
+    public PassengerCar(string name) : base(name) { }
+
+    public override void ShowStatus()
+    {
+        Console.WriteLine($"Легковой {Name}: {Math.Min(Position, 100):F1} км");
+    }
+}
+
+// Грузовой автомобиль
+class Truck : Car
+{
+    public Truck(string name) : base(name) { }
+
+    public override void ShowStatus()
+    {
+        Console.WriteLine($"Грузовой {Name}: {Math.Min(Position, 100):F1} км");
+    }
+}
+
+// Автобус
+class Bus : Car
+{
+    public Bus(string name) : base(name) { }
+
+    public override void ShowStatus()
+    {
+        Console.WriteLine($"Автобус {Name}: {Math.Min(Position, 100):F1} км");
+    }
+}
+
+// Класс игры
+class RaceGame
+{
+    private Car[] _cars;
+    private bool _isFinished;
+
+    public RaceGame(params Car[] cars)
+    {
+        _cars = cars;
+        _isFinished = false;
+
+        foreach (Car c in _cars)
+            c.FinishEvent += OnFinish;
+    }
+
+    private void OnFinish(Car winner)
+    {
+        if (!_isFinished)
+        {
+            _isFinished = true;
+            Console.WriteLine($"\n*** ПОБЕДИТЕЛЬ: {winner.Name} ***\n");
+        }
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("ГОНКА НАЧАЛАСЬ!\n");
+
+        while (!_isFinished)
+        {
+            for (int i = 0; i < _cars.Length; i++)
+            {
+                _cars[i].Go();
+                _cars[i].ShowStatus();
+            }
+            Console.WriteLine("------------------------");
+            System.Threading.Thread.Sleep(500);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car[] cars = new Car[]
+        {
+            new SportCar("Ferrari"),
+            new PassengerCar("Toyota"),
+            new Truck("Volvo"),
+            new Bus("Mercedes")
+        };
+
+        RaceGame game = new RaceGame(cars);
+        game.Start();
+    }
+}
